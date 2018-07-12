168e19ec6377839f6e9684eba59517737b2d6c63
Kim Dean <dea13004@byui.edu>
2018-07-12 17:52:53 -0600

    Lesson 4: Custom Directives

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Document} from "../document.model";
 
 @Component({
@@ -7,11 +7,22 @@ import {Document} from "../document.model";
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
-  documents: Document[];
+  documents: Document[] = [
+    new Document(1, 'Document One', 'This document is a test file', 'www.document1.com', null),
+    new Document(2, 'Document 2', 'This is document dos', 'www.document2.com', null),
+    new Document(3, 'Document III', 'DUMMY DOCUMENT THREE', 'www.document3.com', null),
+    new Document(4, 'Document QUATRO', 'Yet another dummy document', 'www.document4.com', null),
+    new Document(5, 'Document 5', 'Fifth Test doc...', 'www.document5.com', null),
+  ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
